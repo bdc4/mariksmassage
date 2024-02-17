5a5c668a2732f4ccaaab0d87cc7c1f4ce959d607
bdc4 <17170670+bdc4@users.noreply.github.com>
2024-02-17 14:03:56 -0800

    first draft

---
+++ mariksmassage/src/components/MMBookSquare.vue
@@ -0,0 +1,144 @@
+<template>
+  <div>
+    <p>
+      When choosing a service below, you will be redirected to Square™️ to complete your payment.
+      Once you're done, you will recieve a confirmation code for the service you selected.
+    </p>
+    <br />
+    <p><i>
+        Please schedule your appointment ahead of time, and bring your confirmation code, along with a photo ID that shows your first and last
+        name.
+      </i></p>
+    <br />
+    <p>You can schedule a date and time by:</p>
+    <div class="d-flex flex-column align-center my-4">
+      <v-btn variant="text" prepend-icon="mdi-instagram" color="rgb(172, 43, 172)"
+        href="https://www.instagram.com/direct/t/17846127986945126" target="_blank">messaging me
+        on Instagram</v-btn>
+      <div>OR</div>
+      <v-btn variant="text" color="primary" prepend-icon="mdi-phone" href="tel:5093684368">calling my office</v-btn>
+    </div>
+
+    <!-- PAYMENT BUTTONS -->
+    <v-sheet rounded border elevation="100">
+    <v-row>
+      <v-col cols-md="4" class="pa-0">
+        <!-- 90 MINUTE MASSAGE -->
+        <div style="
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  box-shadow: -2px 10px 5px rgba(0, 0, 0, 0);
+  border-radius: 10px;
+  font-family: SQ Market, SQ Market, Helvetica, Arial, sans-serif;
+  ">
+          <div style="padding: 20px;">
+            <p style="
+    font-size: 18px;
+    line-height: 20px;
+  ">90 Minute Massage - In Office</p>
+            <p style="
+    font-size: 18px;
+    line-height: 20px;
+    font-weight: 600;
+  ">$105.00</p>
+            <a target="_blank" href="https://square.link/u/airQkIil?src=embed" style="
+    display: inline-block;
+    font-size: 18px;
+    line-height: 48px;
+    height: 48px;
+    color: #ffffff;
+    min-width: 212px;
+    background-color: #006aff;
+    text-align: center;
+    box-shadow: 0 0 0 1px rgba(0,0,0,.1) inset;
+    border-radius: 6px;
+  ">Book Now</a>
+          </div>
+        </div>
+      </v-col>
+      <v-col cols-md="4" class="pa-0">
+        <!-- 60 MINUTE MASSAGE -->
+        <div style="
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  box-shadow: -2px 10px 5px rgba(0, 0, 0, 0);
+  border-radius: 10px;
+  font-family: SQ Market, SQ Market, Helvetica, Arial, sans-serif;
+  ">
+          <div style="padding: 20px;">
+            <p style="
+    font-size: 18px;
+    line-height: 20px;
+  ">60 Minute Massage - In Office</p>
+            <p style="
+    font-size: 18px;
+    line-height: 20px;
+    font-weight: 600;
+  ">$75.00</p>
+            <a target="_blank" href="https://square.link/u/yQtuYgi2?src=embed" style="
+    display: inline-block;
+    font-size: 18px;
+    line-height: 48px;
+    height: 48px;
+    color: #ffffff;
+    min-width: 212px;
+    background-color: #006aff;
+    text-align: center;
+    box-shadow: 0 0 0 1px rgba(0,0,0,.1) inset;
+    border-radius: 6px;
+  ">Book Now</a>
+          </div>
+        </div>
+      </v-col>
+      <v-col cols-md="4" class="pa-0">
+        <!-- 40 MINUTE MASSAGE -->
+        <div style="
+  overflow: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  box-shadow: -2px 10px 5px rgba(0, 0, 0, 0);
+  border-radius: 10px;
+  font-family: SQ Market, SQ Market, Helvetica, Arial, sans-serif;
+  ">
+          <div style="padding: 20px;">
+            <p style="
+    font-size: 18px;
+    line-height: 20px;
+  ">40 Minute Massage - In Office</p>
+            <p style="
+    font-size: 18px;
+    line-height: 20px;
+    font-weight: 600;
+  ">$55.00</p>
+            <a target="_blank" href="https://square.link/u/mWJBVDmX?src=embed" style="
+    display: inline-block;
+    font-size: 18px;
+    line-height: 48px;
+    height: 48px;
+    color: #ffffff;
+    min-width: 212px;
+    background-color: #006aff;
+    text-align: center;
+    box-shadow: 0 0 0 1px rgba(0,0,0,.1) inset;
+    border-radius: 6px;
+  ">Book Now</a>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+    <div class="text-center">
+      <p><i>*Prices subject to change. To schedule a Mobile Massage, please contact me in advance before booking.</i></p>
+    </div>
+  </v-sheet>
+    
+
+  </div>
+</template>
